a644d7fb65f65aaf171d3e7ce7b51a67046db83e
hankehly <henry.ehly@gmail.com>
2023-12-16 13:22:38 +0900

    Add support for Array fields

--- JRDBDataParsingTools/parse.py
+++ JRDBDataParsingTools/parse.py
@@ -2,20 +2,46 @@ import yaml
 from typing import List
 from pydantic import BaseModel, TypeAdapter
 from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 
 
 class FieldModel(BaseModel):
     relative: int
     byte_length: int
     name: str
+    repeat_factor: int
+    comments: str
 
 
-def parse_line(line, schema):
+def create_pyspark_schema(schema: List[FieldModel]) -> StructType:
+    fields = []
+    for field in schema:
+        field_name = field.name
+        metadata = {"comments": field.comments}
+        if field.repeat_factor > 1:
+            data_type = ArrayType(StringType(), True)
+        else:
+            data_type = StringType()
+        fields.append(StructField(field_name, data_type, True, metadata))
+    return StructType(fields)
+
+
+def parse_line(line, schema: List[FieldModel]):
     parsed_fields = []
     for field in schema:
-        start = field.relative - 1  # Adjust for zero-based indexing
-        end = start + field.byte_length
-        parsed_fields.append(line[start:end].decode("cp932").strip())
+        if field.repeat_factor == 1:
+            start = field.relative - 1  # Adjust for zero-based indexing
+            end = start + field.byte_length
+            parsed_fields.append(line[start:end].decode("cp932").strip())
+        else:
+            repeated_fields = []
+            start = field.relative - 1
+            for _ in range(field.repeat_factor):
+                end = start + field.byte_length
+                repeated_fields.append(line[start:end].decode("cp932").strip())
+                start = end  # Update start for next iteration
+            parsed_fields.append(repeated_fields)
+
     return Row(*parsed_fields)
 
 
@@ -26,6 +52,5 @@ def decode_cp932(line):
 def load_schema(file_path: str) -> List[FieldModel]:
     with open(file_path, "r") as file:
         schema_data = yaml.safe_load(file)
-
     schema = TypeAdapter(List[FieldModel]).validate_python(schema_data)
     return schema
