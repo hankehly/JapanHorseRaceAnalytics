6f1f97a10b7e405332264e6d37de728100a7b95b
hankehly <henry.ehly@gmail.com>
2023-12-16 20:02:38 +0900

    Remove sub-schema implementation

--- JRDBDataParsingTools/data_schema.py
+++ JRDBDataParsingTools/data_schema.py
@@ -4,24 +4,12 @@ from pydantic import BaseModel, TypeAdapter
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 
 
-class SubFieldModel(BaseModel):
-    name: str
-    byte_length: int
-
-
 class FieldModel(BaseModel):
     relative: int
     byte_length: int
     name: str
     repeat_factor: int
     comments: str
-    struct_schema: List[SubFieldModel] = None
-
-
-def create_struct_type(subfields: List[SubFieldModel]) -> StructType:
-    return StructType(
-        [StructField(subfield.name, StringType(), True) for subfield in subfields]
-    )
 
 
 def create_pyspark_schema(schema: List[FieldModel]) -> StructType:
@@ -30,10 +18,7 @@ def create_pyspark_schema(schema: List[FieldModel]) -> StructType:
         field_name = field.name
         metadata = {"comments": field.comments}
         if field.repeat_factor > 1:
-            if field.struct_schema is not None:
-                data_type = ArrayType(create_struct_type(field.struct_schema), True)
-            else:
-                data_type = ArrayType(StringType(), True)
+            data_type = ArrayType(StringType(), True)
         else:
             data_type = StringType()
         fields.append(StructField(field_name, data_type, True, metadata))
