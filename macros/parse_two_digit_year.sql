492ada3b92622caa5be988e7d5cada51f9f339bd
hankehly <henry.ehly@gmail.com>
2023-12-17 17:12:31 +0900

    Add null checks to stg_jrdb__bac model

--- macros/parse_two_digit_year.sql
+++
@@ -1,8 +0,0 @@
-{% macro parse_two_digit_year(year_str) %}
-    -- This macro converts a two-digit year string to a four-digit year integer.
-    -- It assumes a cutoff of 69, so that 69-99 are interpreted as 1969-1999 and 00-68 are interpreted as 2000-2068.
-    case
-        when {{ year_str }}::int < 69 then {{ year_str }}::int + 2000
-        else {{ year_str }}::int + 1900
-    end
-{% endmacro %}
