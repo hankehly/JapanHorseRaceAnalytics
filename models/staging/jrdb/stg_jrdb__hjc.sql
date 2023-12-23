d29ee1d5de53c7ac61692204dac0808f0739fca5
hankehly <henry.ehly@gmail.com>
2023-12-23 20:38:30 +0900

    Add stg_jrdb__hjc model

---
+++ models/staging/jrdb/stg_jrdb__hjc.sql
@@ -0,0 +1,28 @@
+with source as (
+      select * from {{ source('jrdb', 'hjc') }}
+),
+final as (
+    select
+        concat(
+            nullif("レースキー_場コード", ''),
+            nullif("レースキー_年", ''),
+            nullif("レースキー_回", ''),
+            nullif("レースキー_日", ''),
+            nullif("レースキー_Ｒ", '')
+        ) as "レースキー",
+        nullif("レースキー_場コード", '') as "レースキー_場コード",
+        nullif("レースキー_年", '') as "レースキー_年",
+        nullif("レースキー_回", '') as "レースキー_回",
+        nullif("レースキー_日", '') as "レースキー_日",
+        nullif("レースキー_Ｒ", '') as "レースキー_Ｒ",
+        coalesce("単勝払戻", '{}') as "単勝払戻",
+        coalesce("複勝払戻", '{}') as "複勝払戻",
+        coalesce("枠連払戻", '{}') as "枠連払戻",
+        coalesce("馬連払戻", '{}') as "馬連払戻",
+        coalesce("ワイド払戻", '{}') as "ワイド払戻",
+        coalesce("馬単払戻", '{}') as "馬単払戻",
+        coalesce("３連複払戻", '{}') as "３連複払戻",
+        coalesce("３連単払戻", '{}') as "３連単払戻"
+    from source
+)
+select * from final
