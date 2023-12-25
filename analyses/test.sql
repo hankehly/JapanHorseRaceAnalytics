7f6873111f2e42bcbf7d1e1b282350d4ec6132fb
hankehly <henry.ehly@gmail.com>
2023-12-25 16:47:50 -0600

    Add fields to payouts

--- analyses/test.sql
+++ analyses/test.sql
@@ -3,9 +3,9 @@ with
   select
     *
   from
-	jrdb_curated.payouts
+	  jrdb_curated.payouts
   where
-	直前_ＩＤＭ > 15
+	  直前_ＩＤＭ > 15
   ),
 
   hits as (
@@ -19,11 +19,11 @@ with
 
   select
     (select count(*) from bets) total_bets,
-	count(*) total_hits,
+	  count(*) total_hits,
     round(cast(count(*) as numeric) / (select count(*) from bets), 5) hit_rate,
     round(cast((select count(*) from bets) as numeric) / (select count(*) from jrdb_curated.payouts), 5) bet_rate,
-	(select count(*) * 100 from bets) amount_bet,
-	sum(単勝払戻金) amount_won,
-	round(cast(sum(単勝払戻金) as numeric) / (select count(*) * 100 from bets), 5) return_rate
+	  (select count(*) * 100 from bets) amount_bet,
+	  sum(単勝払戻金) amount_won,
+	  round(cast(sum(単勝払戻金) as numeric) / (select count(*) * 100 from bets), 5) return_rate
   from
     hits
--- models/curated/payouts.sql
+++ models/curated/payouts.sql
@@ -1,5 +1,12 @@
 {{ config(materialized='table') }}
 with
+  bac as (
+  select
+    *
+  from
+    {{ ref('stg_jrdb__bac') }}
+  ),
+
   kyi as (
   select
     *
@@ -12,6 +19,13 @@ with
     *
   from
     {{ ref('stg_jrdb__tyb') }}
+  ),
+
+  kab as (
+  select
+    *
+  from
+    {{ ref('stg_jrdb__kab') }}
   ),
 
   win_payouts as (
@@ -30,8 +44,14 @@ with
 
   final as (
   select
-    kyi.レースキー as レースキー,
-    kyi.馬番 as 馬番,
+    kyi."レースキー" as "レースキー",
+    kyi."馬番" as "馬番",
+    bac."年月日" as "年月日",
+
+    bac."レース条件_トラック情報_芝ダ障害コード" as "トラック種別",
+
+    kab."芝馬場差" as "前日_芝馬場差",
+    kab."ダ馬場差" as "前日_ダ馬場差",
 
     kyi."ＩＤＭ" as "前日_ＩＤＭ",
 
@@ -41,22 +61,33 @@ with
     tyb."オッズ指数" as "直前_オッズ指数",
     tyb."パドック指数" as "直前_パドック指数",
 
-    coalesce(win_payouts.払戻金, 0) as 単勝払戻金,
-    coalesce(place_payouts.払戻金, 0) as 複勝払戻金
+    coalesce(win_payouts."払戻金", 0) as "単勝払戻金",
+    coalesce(place_payouts."払戻金", 0) as "複勝払戻金"
   from
     kyi
   inner join
+    bac
+  on
+    kyi."レースキー" = bac."レースキー"
+  inner join
     tyb
-  using
-    (レースキー, 馬番)
+  on
+    kyi."レースキー" = tyb."レースキー"
+    and kyi."馬番" = tyb."馬番"
+  inner join
+    kab
+  on
+    kyi."開催キー" = kab."開催キー"
   left join
     win_payouts
-  using
-    (レースキー, 馬番)
+  on
+    kyi."レースキー" = win_payouts."レースキー"
+    and kyi."馬番" = win_payouts."馬番"
   left join
     place_payouts
-  using
-    (レースキー, 馬番)
+  on
+    kyi."レースキー" = place_payouts."レースキー"
+    and kyi."馬番" = place_payouts."馬番"
   )
   
 select * from final
