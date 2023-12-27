cdb5f37a1718a89def379b152d527b47792d7ab9
hankehly <henry.ehly@gmail.com>
2023-12-27 11:29:38 -0600

    Handle SED duplicates

--- models/curated/features.sql
+++ models/curated/features.sql
@@ -85,6 +85,9 @@ with
     kab."場名",
     bac."年月日",
     kyi."レースキー_場コード" as "場コード",
+    sed."騎手コード",
+    sed."調教師コード",
+    sed."レースキー_Ｒ",
 
     case when extract(month from bac."年月日") <= 3 then 1
          when extract(month from bac."年月日") <= 6 then 2
@@ -100,6 +103,8 @@ with
     sed."レース条件_距離" as "距離",
     skb."馬具コード",
     sed."レース条件_馬場状態" as "馬場状態",
+    sed."本賞金",
+    sed."収得賞金",
 
     bac."頭数",
     bac."レース条件_トラック情報_芝ダ障害コード" as "トラック種別",
@@ -128,6 +133,10 @@ with
     (SELECT "weather_condition" FROM {{ ref('天候コード') }} WHERE "code" = tyb."天候コード") as "直前_天候",
     tyb."単勝オッズ" as "直前_単勝オッズ",
     tyb."複勝オッズ" as "直前_複勝オッズ",
+
+    lag(sed."馬成績_着順", 1) over (partition by kyi."血統登録番号" order by bac."年月日") as "前走着順",
+    lag(sed."馬成績_着順", 2) over (partition by kyi."血統登録番号" order by bac."年月日") as "前々走着順",
+    lag(sed."馬成績_着順", 3) over (partition by kyi."血統登録番号" order by bac."年月日") as "前々々走着順",
 
     coalesce(win_payouts."払戻金", 0) > 0 as "単勝的中",
     coalesce(win_payouts."払戻金", 0) as "単勝払戻金",
@@ -198,6 +207,7 @@ with
   ),
 
   final as (
+  select
     -- 基本情報
     "レースキー",
     "馬番",
@@ -206,6 +216,44 @@ with
     "年月日",
     "頭数",
     "四半期",
+
+    -- 結果/払戻金
+    "単勝的中",
+    "単勝払戻金",
+    "複勝的中",
+    "複勝払戻金",
+    "本賞金",
+    "収得賞金",
+
+    -- 馬情報
+    "血統登録番号",
+    "瞬発戦好走馬_芝",
+    "消耗戦好走馬_芝",
+    "瞬発戦好走馬_ダート",
+    "消耗戦好走馬_ダート",
+    "瞬発戦好走馬_総合",
+    "消耗戦好走馬_総合",
+    "性別",
+
+    -- 前日
+    "トラック種別",
+    "前日_芝馬場差",
+    "前日_ダ馬場差",
+    "前日_ＩＤＭ",
+    "前日_脚質",
+    "前日_単勝オッズ",
+    "前日_複勝オッズ",
+
+    -- 直前
+    "直前_ＩＤＭ",
+    "直前_騎手指数",
+    "直前_情報指数",
+    "直前_オッズ指数",
+    "直前_パドック指数",
+    "直前_脚元情報",
+    "直前_天候",
+    "直前_単勝オッズ",
+    "直前_複勝オッズ",
 
     -- 計算特徴料
     -- https://github.com/codeworks-data/mvp-horse-racing-prediction/blob/master/extract_features.py#L73
@@ -225,6 +273,10 @@ with
     "距離" as "距離", -- distance
     "距離" - lag("距離") over (partition by "血統登録番号" order by "年月日") as "前走距離差", -- diff_distance
     "馬具コード", -- horse_gear
+    age("年月日", "生年月日") as "年齢",
+    age("年月日", "生年月日") < '5 years' as "4歳以下",
+    sum(case when age("年月日", "生年月日") < '5 years' then 1 else 0 end) over (partition by "レースキー") as "4歳以下頭数",
+    sum(case when age("年月日", "生年月日") < '5 years' then 1 else 0 end) over (partition by "レースキー") / "頭数" as "4歳以下割合",
 
     -- how many races this horse has run until now
     count(*) over (partition by "血統登録番号" order by "年月日") - 1 as "レース数", -- horse_runs
@@ -311,6 +363,22 @@ with
     -- horse_going_places
     sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "馬場状態" order by "年月日") - cast("複勝的中" as integer) as "馬場状態トップ3完走",
 
+    -- ratio_win_horse_going
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "血統登録番号", "馬場状態" order by "年月日") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "馬場状態" order by "年月日") - 1 as numeric)'
+      )
+    }} as "馬場状態1位完走率",
+
+    -- ratio_place_horse_going
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "馬場状態" order by "年月日") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "馬場状態" order by "年月日") - 1 as numeric)'
+      )
+    }} as "馬場状態トップ3完走率",
+
     -- horse_distance_runs
     count(*) over (partition by "血統登録番号", "距離" order by "年月日") - 1 as "距離レース数",
 
@@ -319,6 +387,22 @@ with
 
     -- horse_distance_places
     sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "距離" order by "年月日") - cast("複勝的中" as integer) as "距離トップ3完走",
+
+    -- ratio_win_horse_distance
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "血統登録番号", "距離" order by "年月日") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "距離" order by "年月日") - 1 as numeric)'
+      )
+    }} as "距離1位完走率",
+
+    -- ratio_place_horse_distance
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "距離" order by "年月日") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "距離" order by "年月日") - 1 as numeric)'
+      )
+    }} as "距離トップ3完走率",
 
     -- horse_quarter_runs
     count(*) over (partition by "血統登録番号", "四半期" order by "年月日") - 1 as "四半期レース数",
@@ -329,46 +413,188 @@ with
     -- horse_quarter_places
     sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "四半期" order by "年月日") - cast("複勝的中" as integer) as "四半期トップ3完走",
 
-    age("年月日", "生年月日") as "年齢",
-    age("年月日", "生年月日") < '5 years' as "4歳以下",
-    sum(case when age("年月日", "生年月日") < '5 years' then 1 else 0 end) over (partition by "レースキー") as "4歳以下頭数",
-    sum(case when age("年月日", "生年月日") < '5 years' then 1 else 0 end) over (partition by "レースキー") / "頭数" as "4歳以下割合",
+    -- ratio_win_horse_quarter
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "血統登録番号", "四半期" order by "年月日") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "四半期" order by "年月日") - 1 as numeric)'
+      )
+    }} as "四半期1位完走率",
 
-    -- 馬情報
-    "血統登録番号",
-    "瞬発戦好走馬_芝",
-    "消耗戦好走馬_芝",
-    "瞬発戦好走馬_ダート",
-    "消耗戦好走馬_ダート",
-    "瞬発戦好走馬_総合",
-    "消耗戦好走馬_総合",
-    "性別",
+    -- ratio_place_horse_quarter
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号", "四半期" order by "年月日") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号", "四半期" order by "年月日") - 1 as numeric)'
+      )
+    }} as "四半期トップ3完走率",
 
-    -- 前日
-    "トラック種別",
-    "前日_芝馬場差",
-    "前日_ダ馬場差",
-    "前日_ＩＤＭ",
-    "前日_脚質",
-    "前日_単勝オッズ",
-    "前日_複勝オッズ",
+    -- jockey_runs
+    count(*) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - 1 as "騎手レース数",
 
-    -- 直前
-    "直前_ＩＤＭ",
-    "直前_騎手指数",
-    "直前_情報指数",
-    "直前_オッズ指数",
-    "直前_パドック指数",
-    "直前_脚元情報",
-    "直前_天候",
-    "直前_単勝オッズ",
-    "直前_複勝オッズ",
+    -- jockey_wins
+    sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer) as "騎手1位完走",
 
-    -- 結果/払戻金
-    "単勝的中",
-    "単勝払戻金",
-    "複勝的中",
-    "複勝払戻金"
+    -- jockey_places
+    sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer) as "騎手トップ3完走",
+
+    -- ratio_win_jockey
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手1位完走率",
+
+    -- ratio_place_jockey
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手トップ3完走率",
+
+    -- jockey_venue_runs
+    count(*) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as "騎手場所レース数",
+
+    -- jockey_venue_wins
+    sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer) as "騎手場所1位完走",
+
+    -- jockey_venue_places
+    sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer) as "騎手場所トップ3完走",
+
+    -- ratio_win_jockey_venue
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手場所1位完走率",
+
+    -- ratio_place_jockey_venue
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手場所トップ3完走率",
+
+    -- jockey_distance_runs
+    count(*) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - 1 as "騎手距離レース数",
+
+    -- jockey_distance_wins
+    sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer) as "騎手距離1位完走",
+
+    -- jockey_distance_places
+    sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer) as "騎手距離トップ3完走",
+
+    -- ratio_win_jockey_distance
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手距離1位完走率",
+
+    -- ratio_place_jockey_distance
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード", "距離" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "騎手距離トップ3完走率",
+
+    -- trainer_runs
+    count(*) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - 1 as "調教師レース数",
+
+    -- trainer_wins
+    sum(case when "着順" = 1 then 1 else 0 end) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer) as "調教師1位完走",
+
+    -- trainer_places
+    sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer) as "調教師トップ3完走",
+
+    -- ratio_win_trainer
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "調教師1位完走率",
+
+    -- ratio_place_trainer
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "調教師コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "調教師トップ3完走率",
+
+    -- trainer_venue_runs
+    count(*) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as "調教師場所レース数",
+
+    -- trainer_venue_wins
+    sum(case when "着順" = 1 then 1 else 0 end) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer) as "調教師場所1位完走",
+
+    -- trainer_venue_places
+    sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer) as "調教師場所トップ3完走",
+
+    -- ratio_win_trainer_venue
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "調教師場所1位完走率",
+
+    -- ratio_place_trainer_venue
+    {{ 
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "調教師コード", "場コード" order by "年月日", "レースキー_Ｒ") - 1 as numeric)'
+      )
+    }} as "調教師場所トップ3完走率",
+
+    -- Compute the standard rank of the horse on his last 3 races giving us an overview of his state of form
+    coalesce(power(前走着順 - 1, 2) + power(前々走着順 - 1, 2) + power(前々々走着順 - 1, 2), 0) as "過去3走順位平方和", -- horse_std_rank
+
+
+    -- Todo:
+    -- https://teddykoker.com/2019/12/beating-the-odds-machine-learning-for-horse-racing/
+    
+    -- Horse Win Percent: Horse’s win percent over the past 5 races.
+    -- horse_win_percent_past_5_races
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "血統登録番号" order by "年月日" rows between 5 preceding and 1 preceding) - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号" order by "年月日" rows between 5 preceding and 1 preceding) - 1 as numeric)'
+      )
+    }} as "過去5走勝率",
+
+    -- horse_place_percent_past_5_races
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "血統登録番号" order by "年月日" rows between 5 preceding and 1 preceding) - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "血統登録番号" order by "年月日" rows between 5 preceding and 1 preceding) - 1 as numeric)'
+      )
+    }} as "過去5走トップ3完走率",
+
+    -- Jockey Win Percent: Jockey’s win percent over the past 5 races.
+    -- jockey_win_percent_past_5_races
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" = 1 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ" rows between 5 preceding and 1 preceding) - cast("単勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ" rows between 5 preceding and 1 preceding) - 1 as numeric)'
+      )
+    }} as "騎手過去5走勝率",
+
+    -- jockey_place_percent_past_5_races
+    {{
+      dbt_utils.safe_divide(
+        'sum(case when "着順" <= 3 then 1 else 0 end) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ" rows between 5 preceding and 1 preceding) - cast("複勝的中" as integer)',
+        'cast(count(*) over (partition by "騎手コード" order by "年月日", "レースキー_Ｒ" rows between 5 preceding and 1 preceding) - 1 as numeric)'
+      )
+    }} as "騎手過去5走トップ3完走率",
+
   from
     base
   )
