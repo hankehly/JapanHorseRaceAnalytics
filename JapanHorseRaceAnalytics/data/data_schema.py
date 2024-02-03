e6691578e6d1f3baf0c2a6d89f340d705c05150c
hankehly <henry.ehly@gmail.com>
2024-02-03 17:17:34 +0900

    Update ignore path

---
+++ JapanHorseRaceAnalytics/data/data_schema.py
@@ -0,0 +1,32 @@
+import yaml
+from typing import List
+from pydantic import BaseModel, TypeAdapter
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
+
+
+class FieldModel(BaseModel):
+    relative: int
+    byte_length: int
+    name: str
+    repeat_factor: int
+    comments: str
+
+
+def create_pyspark_schema(schema: List[FieldModel]) -> StructType:
+    fields = []
+    for field in schema:
+        field_name = field.name
+        metadata = {"comments": field.comments}
+        if field.repeat_factor > 1:
+            data_type = ArrayType(StringType(), True)
+        else:
+            data_type = StringType()
+        fields.append(StructField(field_name, data_type, True, metadata))
+    return StructType(fields)
+
+
+def load_schema(file_path: str) -> List[FieldModel]:
+    with open(file_path, "r") as file:
+        schema_data = yaml.safe_load(file)
+    schema = TypeAdapter(List[FieldModel]).validate_python(schema_data)
+    return schema
