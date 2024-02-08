fcb04b638890df7f6abafe3c75829ac778c9073b
hankehly <henry.ehly@gmail.com>
2024-02-09 08:48:15 +0900

    Update UKC duplicate analysis file

--- analyses/find_duplicates_ukc.sql
+++ analyses/find_duplicates_ukc.sql
@@ -1,29 +1,72 @@
+-- Check for duplicate keys.
+-- with
+--   duplicates as (
+--   select
+--     `血統登録番号`,
+-- 	  `データ年月日`,
+--     count(*)
+--   from
+--     {{ source('jrdb', 'raw_jrdb__ukc') }}
+--   group by
+--     `血統登録番号`,
+--     `データ年月日`
+--   having
+--     count(*) > 1
+--   )
+-- select
+--   *
+-- from
+--   {{ source('jrdb', 'raw_jrdb__ukc') }}
+-- where
+--   (`血統登録番号`, `データ年月日`) in (select `血統登録番号`, `データ年月日` from duplicates)
+-- order by
+--   `血統登録番号`, `データ年月日`
+
+-- Check for duplicate keys with different data.
 with
+  distinct_rows as (
+  -- removed surrogate key which is always unique
+  select distinct
+    `血統登録番号`,
+    `馬名`,
+    `性別コード`,
+    `毛色コード`,
+    `馬記号コード`,
+    `血統情報_父馬名`,
+    `血統情報_母馬名`,
+    `血統情報_母父馬名`,
+    `生年月日`,
+    `父馬生年`,
+    `母馬生年`,
+    `母父馬生年`,
+    `馬主名`,
+    `馬主会コード`,
+    `生産者名`,
+    `産地名`,
+    `登録抹消フラグ`,
+    `データ年月日`,
+    `父系統コード`,
+    `母父系統コード`,
+  from
+    {{ source('jrdb', 'raw_jrdb__ukc') }}
+  ),
   duplicates as (
   select
     `血統登録番号`,
-	`データ年月日`,
-    count(*)
+    `データ年月日`
   from
-    jhra_raw.raw_jrdb__ukc
+    distinct_rows
   group by
     `血統登録番号`,
     `データ年月日`
   having
     count(*) > 1
-  ),
-  duplicates_with_sk as (
-  select
-    row_number() over (partition by `血統登録番号`, `データ年月日` order by ukc_sk) rn,
-    *
-  from
-    jhra_raw.raw_jrdb__ukc
-  where
-    (`血統登録番号`, `データ年月日`) in (select `血統登録番号`, `データ年月日` from duplicates)
   )
-
--- The following query will return all duplicate rows.
 select
   *
 from
-  duplicates_with_sk
+  {{ source('jrdb', 'raw_jrdb__ukc') }}
+where
+  (`血統登録番号`, `データ年月日`) in (select `血統登録番号`, `データ年月日` from duplicates)
+order by
+  `血統登録番号`, `データ年月日`
