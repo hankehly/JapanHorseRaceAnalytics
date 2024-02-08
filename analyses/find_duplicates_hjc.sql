5ad2a69c06106acf3e63c3e930f6485db22e90ff
hankehly <henry.ehly@gmail.com>
2024-02-09 08:03:41 +0900

    Update hjc duplicate check

--- analyses/find_duplicates_hjc.sql
+++ analyses/find_duplicates_hjc.sql
@@ -8,7 +8,7 @@ with
     `レースキー_Ｒ`,
     count(*)
   from
-    jhra_raw.raw_jrdb__hjc
+    {{ source('jrdb', 'raw_jrdb__hjc') }}
   group by
     `レースキー_場コード`,
     `レースキー_年`,
@@ -17,18 +17,12 @@ with
     `レースキー_Ｒ`
   having
     count(*) > 1
-  ),
-  duplicates_with_sk as (
-  select
-    row_number() over (partition by `レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ` order by hjc_sk desc) rn,
-    *
-  from
-    jhra_raw.raw_jrdb__hjc
-  where
-    (`レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ`) in (select `レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ` from duplicates)
   )
-
 select
   *
 from
-  duplicates_with_sk
+  {{ source('jrdb', 'raw_jrdb__hjc') }}
+where
+  (`レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ`) in (select `レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ` from duplicates)
+order by
+  `レースキー_場コード`, `レースキー_年`, `レースキー_回`, `レースキー_日`, `レースキー_Ｒ`
--- models/staging/jrdb/stg_jrdb__hjc.sql
+++ models/staging/jrdb/stg_jrdb__hjc.sql
@@ -1,42 +1,47 @@
-with source as (
-      select * from {{ source('jrdb', 'raw_jrdb__hjc') }}
-),
-final as (
-    select
-        hjc_sk,
-        concat(
-            nullif(`レースキー_場コード`, ''),
-            nullif(`レースキー_年`, ''),
-            nullif(`レースキー_回`, ''),
-            nullif(`レースキー_日`, ''),
-            nullif(`レースキー_Ｒ`, '')
-        ) as hjc_bk,
-        concat(
-            nullif(`レースキー_場コード`, ''),
-            nullif(`レースキー_年`, ''),
-            nullif(`レースキー_回`, ''),
-            nullif(`レースキー_日`, ''),
-            nullif(`レースキー_Ｒ`, '')
-        ) as `レースキー`,
-        concat(
-            nullif(`レースキー_場コード`, ''),
-            nullif(`レースキー_年`, ''),
-            nullif(`レースキー_回`, ''),
-            nullif(`レースキー_日`, '')
-        ) as `開催キー`,
-        nullif(`レースキー_場コード`, '') as `レースキー_場コード`,
-        nullif(`レースキー_年`, '') as `レースキー_年`,
-        nullif(`レースキー_回`, '') as `レースキー_回`,
-        nullif(`レースキー_日`, '') as `レースキー_日`,
-        nullif(`レースキー_Ｒ`, '') as `レースキー_Ｒ`,
-        `単勝払戻`,
-        `複勝払戻`,
-        `枠連払戻`,
-        `馬連払戻`,
-        `ワイド払戻`,
-        `馬単払戻`,
-        `３連複払戻`,
-        `３連単払戻`
-    from source
-)
+with
+  source as (
+  select
+    *
+  from
+    {{ source('jrdb', 'raw_jrdb__hjc') }}
+  ),
+  final as (
+  select
+    hjc_sk,
+    concat(
+      nullif(`レースキー_場コード`, ''),
+      nullif(`レースキー_年`, ''),
+      nullif(`レースキー_回`, ''),
+      nullif(`レースキー_日`, ''),
+      nullif(`レースキー_Ｒ`, '')
+    ) as hjc_bk,
+    concat(
+      nullif(`レースキー_場コード`, ''),
+      nullif(`レースキー_年`, ''),
+      nullif(`レースキー_回`, ''),
+      nullif(`レースキー_日`, ''),
+      nullif(`レースキー_Ｒ`, '')
+    ) as `レースキー`,
+    concat(
+      nullif(`レースキー_場コード`, ''),
+      nullif(`レースキー_年`, ''),
+      nullif(`レースキー_回`, ''),
+      nullif(`レースキー_日`, '')
+    ) as `開催キー`,
+    nullif(`レースキー_場コード`, '') as `レースキー_場コード`,
+    nullif(`レースキー_年`, '') as `レースキー_年`,
+    nullif(`レースキー_回`, '') as `レースキー_回`,
+    nullif(`レースキー_日`, '') as `レースキー_日`,
+    nullif(`レースキー_Ｒ`, '') as `レースキー_Ｒ`,
+    `単勝払戻`,
+    `複勝払戻`,
+    `枠連払戻`,
+    `馬連払戻`,
+    `ワイド払戻`,
+    `馬単払戻`,
+    `３連複払戻`,
+    `３連単払戻`
+  from
+    source
+  )
 select * from final
