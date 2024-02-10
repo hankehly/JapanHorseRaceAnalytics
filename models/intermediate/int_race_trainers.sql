38236e5a3b24a3e28ffda619157c198a45ae1d84
hankehly <henry.ehly@gmail.com>
2024-02-10 16:55:44 +0900

    Add competitor info to int_race_trainers model

--- models/intermediate/int_race_trainers.sql
+++ models/intermediate/int_race_trainers.sql
@@ -25,16 +25,17 @@ with
     concat(kyi.`レースキー`, kyi.`馬番`) as `unique_key`,
     kyi.`レースキー`,
     kyi.`馬番`,
-    bac.`年月日`,
+    bac.`発走日時`,
     kyi.`レースキー_場コード` as `場コード`,
     kyi.`調教師コード`,
-    cast(kyi.`レースキー_Ｒ` as integer) `レースキー_Ｒ`,
+    cast(kyi.`レースキー_ｒ` as integer) `レースキー_ｒ`,
     sed.`本賞金`,
     sed.`馬成績_着順` as `着順`,
     -- trainer_runs
-    coalesce(cast(count(*) over (partition by kyi.`調教師コード` order by bac.`年月日`, cast(kyi.`レースキー_Ｒ` as integer)) - 1 as integer), 0) as `調教師レース数`,
-    -- trainer_wins
-    coalesce(cast(sum(case when sed.`馬成績_着順` = 1 then 1 else 0 end) over (partition by kyi.`調教師コード` order by bac.`年月日`, cast(kyi.`レースキー_Ｒ` as integer) rows between unbounded preceding and 1 preceding) as integer), 0) as `調教師1位完走`,
+    -- When you have two races on the same date, the -1 logic doesn't work (because it should be -2)
+    -- The group by logic works correctly but you have to find a way to do that same thing with 場コード and others as well (new ctes?)
+    -- coalesce(cast(count(*) over (partition by kyi.`調教師コード` order by bac.`発走日時`) - 1 as integer), 0) as `調教師レース数`,
+    -- coalesce(cast(sum(case when sed.`馬成績_着順` = 1 then 1 else 0 end) over (partition by kyi.`調教師コード` order by bac.`発走日時` rows between unbounded preceding and 1 preceding) as integer), 0) as `調教師1位完走`,
     case when sed.`馬成績_着順` = 1 then 1 else 0 end as is_win,
     case when sed.`馬成績_着順` <= 3 then 1 else 0 end as is_place
   from
@@ -47,212 +48,219 @@ with
     kyi.`レースキー` = bac.`レースキー`
 
   -- 実績系はレースキーがないかもしれないから left join
-  left join
+  inner join
     sed
   on
     kyi.`レースキー` = sed.`レースキー`
     and kyi.`馬番` = sed.`馬番`
   ),
 
-  -- LOOKAHEAD PROBLEM: The same trainer can have multiple horses in the same race.
-  -- So when we calculate how many wins the trainer has has so far, we can't consider horses in the same race.
+  -- lookahead problem: the same trainer can have multiple horses in the same race.
+  -- so when we calculate how many wins the trainer has has so far, we can't consider horses in the same race.
 
+  trainer_counts as (
+  select
+    `調教師コード`,
+    `発走日時`,
+    count(*) as races,
+    sum(is_win) as wins,
+    sum(is_place) as placings,
+    sum(`本賞金`) as `本賞金`,
+    sum(case when is_win = 1 then `本賞金` else 0 end) as `本賞金1着`
+  from
+    race_trainers_base
+  group by
+    `調教師コード`,
+    `発走日時`
+  ),
+
+  -- Do this later..
   -- race_trainers_streaks as (
-  -- select
-  --   `レースキー`,
-  --   `馬番`,
-  --   sum(is_win) over (partition by `調教師コード`, win_group order by `調教師レース数`) as `調教師連続1着`,
-  --   sum(is_place) over (partition by `調教師コード`, place_group order by `調教師レース数`) as `調教師連続3着内`
-  -- from (
-  --   select
-  --     `レースキー`,
-  --     `馬番`,
-  --     `調教師コード`,
-  --     is_win,
-  --     is_place,
-  --     `調教師レース数`,
-  --     sum(case when is_win = 0 then 1 else 0 end) over (partition by `調教師コード` order by `調教師レース数`) as win_group,
-  --     sum(case when is_place = 0 then 1 else 0 end) over (partition by `調教師コード` order by `調教師レース数`) as place_group
-  --   from
-  --     race_trainers_base
-  --   )
+  --
   -- ),
+
+  trainer_cumulatives as (
+  select
+    `発走日時`,
+    `調教師コード`,
+    sum(races) over (partition by `調教師コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師レース数`,
+    sum(wins) over (partition by `調教師コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師1位完走`,
+    sum(placings) over (partition by `調教師コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師トップ3完走`,
+    sum(`本賞金`) over (partition by `調教師コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師本賞金累計`,
+    sum(`本賞金1着`) over (partition by `調教師コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師本賞金1着累計`
+  from
+    trainer_counts
+  ),
+
+  trainer_venue_counts as (
+  select
+    `調教師コード`,
+    `場コード`,
+    `発走日時`,
+    count(*) as races,
+    sum(is_win) as wins,
+    sum(is_place) as placings
+  from
+    race_trainers_base
+  group by
+    `調教師コード`,
+    `場コード`,
+    `発走日時`
+  ),
+
+  trainer_venue_cumulatives as (
+  select
+    `発走日時`,
+    `調教師コード`,
+    `場コード`,
+    sum(races) over (partition by `調教師コード`, `場コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師場所レース数`,
+    sum(wins) over (partition by `調教師コード`, `場コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師場所1位完走`,
+    sum(placings) over (partition by `調教師コード`, `場コード` order by `発走日時` rows between unbounded preceding and 1 preceding) as `調教師場所トップ3完走`
+  from
+    trainer_venue_counts
+  ),
 
   race_trainers as (
   select
-    base.`unique_key`,
-    base.`レースキー`,
-    base.`馬番`,
-    base.`年月日`,
-    base.`レースキー_Ｒ`,
-    base.`調教師コード`,
-    base.`着順` as `先読み注意_着順`,
-    base.`調教師レース数`,
-    base.`調教師1位完走`,
+    -- Metadata fields (not used for prediction)
+    yt.`unique_key`,
+    yt.`レースキー`,
+    yt.`馬番`,
+    yt.`場コード`,
+    yt.`調教師コード`,
+    yt.`発走日時`,
+    yt.`着順` as `先読み注意_着順`,
+    yt.`本賞金` as `先読み注意_本賞金`,
 
-    -- trainer_places
-    coalesce(cast(sum(case when `着順` <= 3 then 1 else 0 end) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding) as integer), 0) as `調教師トップ3完走`,
+    -- Base features
+    coalesce(cp.`調教師レース数`, 0) as `調教師レース数`, -- trainer_runs
+    coalesce(cp.`調教師1位完走`, 0) as `調教師1位完走`, -- trainer_wins
+    coalesce(cp.`調教師トップ3完走`, 0) as `調教師トップ3完走`, -- trainer_places
+    coalesce(cp.`調教師1位完走` / cp.`調教師レース数`, 0) as `調教師1位完走率`, -- ratio_win_trainer
+    coalesce(cp.`調教師トップ3完走` / cp.`調教師レース数`, 0) as `調教師トップ3完走率`, -- ratio_place_trainer
 
-    -- ratio_win_trainer
-    coalesce({{
-      dbt_utils.safe_divide(
-        'sum(case when `着順` = 1 then 1 else 0 end) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        'cast(count(*) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ`) - 1 as float)'
-      )
-    }}, 0) as `調教師1位完走率`,
+    coalesce(tv.`調教師場所レース数`, 0) as `調教師場所レース数`, -- trainer_venue_runs
+    coalesce(tv.`調教師場所1位完走`, 0) as `調教師場所1位完走`, -- trainer_venue_wins
+    coalesce(tv.`調教師場所トップ3完走`, 0) as `調教師場所トップ3完走`, -- trainer_venue_places
+    coalesce(tv.`調教師場所1位完走` / tv.`調教師場所レース数`, 0) as `調教師場所1位完走率`, -- ratio_win_trainer_venue
+    coalesce(tv.`調教師場所トップ3完走` / tv.`調教師場所レース数`, 0) as `調教師場所トップ3完走率`, -- ratio_place_trainer_venue
 
-    -- ratio_place_trainer
-    coalesce({{ 
-      dbt_utils.safe_divide(
-        'sum(case when `着順` <= 3 then 1 else 0 end) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        'cast(count(*) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ`) - 1 as float)'
-      )
-    }}, 0) as `調教師トップ3完走率`,
+    coalesce(cp.`調教師本賞金累計`, 0) as `調教師本賞金累計`, -- prize_trainer_cumulative
 
-    -- trainer_venue_runs
-    coalesce(cast(count(*) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ`) - 1 as integer), 0) as `調教師場所レース数`,
+    -- When you win, how much money do you win on average?
+    coalesce(cp.`調教師本賞金1着累計` / cp.`調教師1位完走`, 0) as `調教師1位完走平均賞金`, -- avg_prize_wins_trainer
+    coalesce(cp.`調教師本賞金累計` / cp.`調教師レース数`, 0) as `調教師レース数平均賞金`  -- avg_prize_runs_trainer
 
-    -- trainer_venue_wins
-    coalesce(cast(sum(case when `着順` = 1 then 1 else 0 end) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding) as integer), 0) as `調教師場所1位完走`,
+    -- Do this later
+    -- 調教師連続1着
+    -- 調教師連続3着内
 
-    -- trainer_venue_places
-    coalesce(cast(sum(case when `着順` <= 3 then 1 else 0 end) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding) as integer), 0) as `調教師場所トップ3完走`,
-
-    -- ratio_win_trainer_venue
-    coalesce({{
-      dbt_utils.safe_divide(
-        'sum(case when `着順` = 1 then 1 else 0 end) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        'cast(count(*) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ`) - 1 as float)'
-      )
-    }}, 0) as `調教師場所1位完走率`,
-
-    -- ratio_place_trainer_venue
-    coalesce({{ 
-      dbt_utils.safe_divide(
-        'sum(case when `着順` <= 3 then 1 else 0 end) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        'cast(count(*) over (partition by `調教師コード`, `場コード` order by `年月日`, `レースキー_Ｒ`) - 1 as float)'
-      )
-    }}, 0) as `調教師場所トップ3完走率`,
-
-    -- prize_trainer_cumulative
-    coalesce(sum(`本賞金`) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding), 0) as `調教師本賞金累計`,
-
-    -- avg_prize_wins_trainer
-    coalesce({{
-      dbt_utils.safe_divide(
-        'sum(`本賞金`) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        '`調教師1位完走`'
-      )
-    }}, 0) as `調教師1位完走平均賞金`,
-
-    -- avg_prize_runs_trainer
-    coalesce({{
-      dbt_utils.safe_divide(
-        'sum(`本賞金`) over (partition by `調教師コード` order by `年月日`, `レースキー_Ｒ` rows between unbounded preceding and 1 preceding)',
-        '`調教師レース数`'
-      )
-    }}, 0) as `調教師レース数平均賞金`,
-
-    lag(race_trainers_streaks.`調教師連続1着`, 1, 0) over (partition by base.`調教師コード` order by `年月日`, `レースキー_Ｒ`) as `調教師連続1着`,
-    lag(race_trainers_streaks.`調教師連続3着内`, 1, 0) over (partition by base.`調教師コード` order by `年月日`, `レースキー_Ｒ`) as `調教師連続3着内`
   from
-    race_trainers_base base
+    race_trainers_base yt
   inner join
-    race_trainers_streaks
+    trainer_cumulatives cp
   on
-    base.`レースキー` = race_trainers_streaks.`レースキー`
-    and base.`馬番` = race_trainers_streaks.`馬番`
+    yt.`発走日時` = cp.`発走日時`
+    and yt.`調教師コード` = cp.`調教師コード`
+  inner join
+    trainer_venue_cumulatives tv
+  on
+    -- Careful with the join here, it's not the same as the previous one
+    -- We need to join on all group by fields to avoid duplicates
+    yt.`発走日時` = tv.`発走日時`
+    and yt.`調教師コード` = tv.`調教師コード`
+    and yt.`場コード` = tv.`場コード`
   ),
 
   competitors as (
   select
     a.`レースキー`,
-    a.`馬番`,
-    -- `調教師レース数`
+    a.`調教師コード`,
+    -- 調教師レース数
     max(b.`調教師レース数`) as `競争相手最高調教師レース数`,
     min(b.`調教師レース数`) as `競争相手最低調教師レース数`,
     avg(b.`調教師レース数`) as `競争相手平均調教師レース数`,
     stddev_pop(b.`調教師レース数`) as `競争相手調教師レース数標準偏差`,
-    -- `調教師1位完走`
+    -- 調教師1位完走
     max(b.`調教師1位完走`) as `競争相手最高調教師1位完走`,
     min(b.`調教師1位完走`) as `競争相手最低調教師1位完走`,
     avg(b.`調教師1位完走`) as `競争相手平均調教師1位完走`,
     stddev_pop(b.`調教師1位完走`) as `競争相手調教師1位完走標準偏差`,
-    -- `調教師トップ3完走`
+    -- 調教師トップ3完走
     max(b.`調教師トップ3完走`) as `競争相手最高調教師トップ3完走`,
     min(b.`調教師トップ3完走`) as `競争相手最低調教師トップ3完走`,
     avg(b.`調教師トップ3完走`) as `競争相手平均調教師トップ3完走`,
     stddev_pop(b.`調教師トップ3完走`) as `競争相手調教師トップ3完走標準偏差`,
-    -- `調教師1位完走率`
+    -- 調教師1位完走率
     max(b.`調教師1位完走率`) as `競争相手最高調教師1位完走率`,
     min(b.`調教師1位完走率`) as `競争相手最低調教師1位完走率`,
     avg(b.`調教師1位完走率`) as `競争相手平均調教師1位完走率`,
     stddev_pop(b.`調教師1位完走率`) as `競争相手調教師1位完走率標準偏差`,
-    -- `調教師トップ3完走率`
+    -- 調教師トップ3完走率
     max(b.`調教師トップ3完走率`) as `競争相手最高調教師トップ3完走率`,
     min(b.`調教師トップ3完走率`) as `競争相手最低調教師トップ3完走率`,
     avg(b.`調教師トップ3完走率`) as `競争相手平均調教師トップ3完走率`,
     stddev_pop(b.`調教師トップ3完走率`) as `競争相手調教師トップ3完走率標準偏差`,
-    -- `調教師場所レース数`
+    -- 調教師場所レース数
     max(b.`調教師場所レース数`) as `競争相手最高調教師場所レース数`,
     min(b.`調教師場所レース数`) as `競争相手最低調教師場所レース数`,
     avg(b.`調教師場所レース数`) as `競争相手平均調教師場所レース数`,
     stddev_pop(b.`調教師場所レース数`) as `競争相手調教師場所レース数標準偏差`,
-    -- `調教師場所1位完走`
+    -- 調教師場所1位完走
     max(b.`調教師場所1位完走`) as `競争相手最高調教師場所1位完走`,
     min(b.`調教師場所1位完走`) as `競争相手最低調教師場所1位完走`,
     avg(b.`調教師場所1位完走`) as `競争相手平均調教師場所1位完走`,
     stddev_pop(b.`調教師場所1位完走`) as `競争相手調教師場所1位完走標準偏差`,
-    -- `調教師場所トップ3完走`
+    -- 調教師場所トップ3完走
     max(b.`調教師場所トップ3完走`) as `競争相手最高調教師場所トップ3完走`,
     min(b.`調教師場所トップ3完走`) as `競争相手最低調教師場所トップ3完走`,
     avg(b.`調教師場所トップ3完走`) as `競争相手平均調教師場所トップ3完走`,
     stddev_pop(b.`調教師場所トップ3完走`) as `競争相手調教師場所トップ3完走標準偏差`,
-    -- `調教師場所1位完走率`
+    -- 調教師場所1位完走率
     max(b.`調教師場所1位完走率`) as `競争相手最高調教師場所1位完走率`,
     min(b.`調教師場所1位完走率`) as `競争相手最低調教師場所1位完走率`,
     avg(b.`調教師場所1位完走率`) as `競争相手平均調教師場所1位完走率`,
     stddev_pop(b.`調教師場所1位完走率`) as `競争相手調教師場所1位完走率標準偏差`,
-    -- `調教師場所トップ3完走率`
+    -- 調教師場所トップ3完走率
     max(b.`調教師場所トップ3完走率`) as `競争相手最高調教師場所トップ3完走率`,
     min(b.`調教師場所トップ3完走率`) as `競争相手最低調教師場所トップ3完走率`,
     avg(b.`調教師場所トップ3完走率`) as `競争相手平均調教師場所トップ3完走率`,
     stddev_pop(b.`調教師場所トップ3完走率`) as `競争相手調教師場所トップ3完走率標準偏差`,
-    -- `調教師本賞金累計`
+    -- 調教師本賞金累計
     max(b.`調教師本賞金累計`) as `競争相手最高調教師本賞金累計`,
     min(b.`調教師本賞金累計`) as `競争相手最低調教師本賞金累計`,
     avg(b.`調教師本賞金累計`) as `競争相手平均調教師本賞金累計`,
     stddev_pop(b.`調教師本賞金累計`) as `競争相手調教師本賞金累計標準偏差`,
-    -- `調教師1位完走平均賞金`
+    -- 調教師1位完走平均賞金
     max(b.`調教師1位完走平均賞金`) as `競争相手最高調教師1位完走平均賞金`,
     min(b.`調教師1位完走平均賞金`) as `競争相手最低調教師1位完走平均賞金`,
     avg(b.`調教師1位完走平均賞金`) as `競争相手平均調教師1位完走平均賞金`,
     stddev_pop(b.`調教師1位完走平均賞金`) as `競争相手調教師1位完走平均賞金標準偏差`,
-    -- `調教師レース数平均賞金`
+    -- 調教師レース数平均賞金
     max(b.`調教師レース数平均賞金`) as `競争相手最高調教師レース数平均賞金`,
     min(b.`調教師レース数平均賞金`) as `競争相手最低調教師レース数平均賞金`,
     avg(b.`調教師レース数平均賞金`) as `競争相手平均調教師レース数平均賞金`,
-    stddev_pop(b.`調教師レース数平均賞金`) as `競争相手調教師レース数平均賞金標準偏差`,
-    -- `調教師連続1着`
-    max(b.`調教師連続1着`) as `競争相手最高調教師連続1着`,
-    min(b.`調教師連続1着`) as `競争相手最低調教師連続1着`,
-    avg(b.`調教師連続1着`) as `競争相手平均調教師連続1着`,
-    stddev_pop(b.`調教師連続1着`) as `競争相手調教師連続1着標準偏差`,
-    -- `調教師連続3着内`
-    max(b.`調教師連続3着内`) as `競争相手最高調教師連続3着内`,
-    min(b.`調教師連続3着内`) as `競争相手最低調教師連続3着内`,
-    avg(b.`調教師連続3着内`) as `競争相手平均調教師連続3着内`,
-    stddev_pop(b.`調教師連続3着内`) as `競争相手調教師連続3着内標準偏差`
+    stddev_pop(b.`調教師レース数平均賞金`) as `競争相手調教師レース数平均賞金標準偏差`
+    -- 調教師連続1着
+    -- max(b.`調教師連続1着`) as `競争相手最高調教師連続1着`,
+    -- min(b.`調教師連続1着`) as `競争相手最低調教師連続1着`,
+    -- avg(b.`調教師連続1着`) as `競争相手平均調教師連続1着`,
+    -- stddev_pop(b.`調教師連続1着`) as `競争相手調教師連続1着標準偏差`,
+    -- 調教師連続3着内
+    -- max(b.`調教師連続3着内`) as `競争相手最高調教師連続3着内`,
+    -- min(b.`調教師連続3着内`) as `競争相手最低調教師連続3着内`,
+    -- avg(b.`調教師連続3着内`) as `競争相手平均調教師連続3着内`,
+    -- stddev_pop(b.`調教師連続3着内`) as `競争相手調教師連続3着内標準偏差`
   from
     race_trainers a
   inner join
     race_trainers b
   on
     a.`レースキー` = b.`レースキー`
-    and a.`馬番` <> b.`馬番`
+    and a.`調教師コード` <> b.`調教師コード`
   group by
     a.`レースキー`,
-    a.`馬番`
+    a.`調教師コード`
   ),
 
   final as (
@@ -261,27 +269,26 @@ with
     race_trainers.`unique_key`,
     race_trainers.`レースキー`,
     race_trainers.`馬番`,
-    race_trainers.`年月日`,
-    race_trainers.`レースキー_Ｒ`,
+    race_trainers.`場コード`,
     race_trainers.`調教師コード`,
+    race_trainers.`発走日時`,
     race_trainers.`先読み注意_着順`,
+    race_trainers.`先読み注意_本賞金`,
 
     -- Base features
-    race_trainers.`調教師レース数`, -- trainer_runs
-    race_trainers.`調教師1位完走`, -- trainer_wins
-    race_trainers.`調教師トップ3完走`, -- trainer_places
-    race_trainers.`調教師1位完走率`, -- ratio_win_trainer
-    race_trainers.`調教師トップ3完走率`, -- ratio_place_trainer
-    race_trainers.`調教師場所レース数`, -- trainer_venue_runs
-    race_trainers.`調教師場所1位完走`, -- trainer_venue_wins
-    race_trainers.`調教師場所トップ3完走`, -- trainer_venue_places
-    race_trainers.`調教師場所1位完走率`, -- ratio_win_trainer_venue
-    race_trainers.`調教師場所トップ3完走率`, -- ratio_place_trainer_venue
-    race_trainers.`調教師本賞金累計`, -- prize_trainer_cumulative
-    race_trainers.`調教師1位完走平均賞金`, -- avg_prize_wins_trainer
-    race_trainers.`調教師レース数平均賞金`, -- avg_prize_runs_trainer
-    race_trainers.`調教師連続1着`,
-    race_trainers.`調教師連続3着内`,
+    race_trainers.`調教師レース数`,
+    race_trainers.`調教師1位完走`,
+    race_trainers.`調教師トップ3完走`,
+    race_trainers.`調教師1位完走率`,
+    race_trainers.`調教師トップ3完走率`,
+    race_trainers.`調教師場所レース数`,
+    race_trainers.`調教師場所1位完走`,
+    race_trainers.`調教師場所トップ3完走`,
+    race_trainers.`調教師場所1位完走率`,
+    race_trainers.`調教師場所トップ3完走率`,
+    race_trainers.`調教師本賞金累計`,
+    race_trainers.`調教師1位完走平均賞金`,
+    race_trainers.`調教師レース数平均賞金`,
 
     -- Competitors features
     competitors.`競争相手最高調教師レース数`,
@@ -336,14 +343,14 @@ with
     competitors.`競争相手最低調教師レース数平均賞金`,
     competitors.`競争相手平均調教師レース数平均賞金`,
     competitors.`競争相手調教師レース数平均賞金標準偏差`,
-    competitors.`競争相手最高調教師連続1着`,
-    competitors.`競争相手最低調教師連続1着`,
-    competitors.`競争相手平均調教師連続1着`,
-    competitors.`競争相手調教師連続1着標準偏差`,
-    competitors.`競争相手最高調教師連続3着内`,
-    competitors.`競争相手最低調教師連続3着内`,
-    competitors.`競争相手平均調教師連続3着内`,
-    competitors.`競争相手調教師連続3着内標準偏差`,
+    -- competitors.`競争相手最高調教師連続1着`,
+    -- competitors.`競争相手最低調教師連続1着`,
+    -- competitors.`競争相手平均調教師連続1着`,
+    -- competitors.`競争相手調教師連続1着標準偏差`,
+    -- competitors.`競争相手最高調教師連続3着内`,
+    -- competitors.`競争相手最低調教師連続3着内`,
+    -- competitors.`競争相手平均調教師連続3着内`,
+    -- competitors.`競争相手調教師連続3着内標準偏差`
 
     -- Relative features
     race_trainers.`調教師レース数` - competitors.`競争相手平均調教師レース数` as `競争相手平均調教師レース数差`,
@@ -359,17 +366,12 @@ with
     race_trainers.`調教師本賞金累計` - competitors.`競争相手平均調教師本賞金累計` as `競争相手平均調教師本賞金累計差`,
     race_trainers.`調教師1位完走平均賞金` - competitors.`競争相手平均調教師1位完走平均賞金` as `競争相手平均調教師1位完走平均賞金差`,
     race_trainers.`調教師レース数平均賞金` - competitors.`競争相手平均調教師レース数平均賞金` as `競争相手平均調教師レース数平均賞金差`
-
   from
     race_trainers
   inner join
     competitors
   on
     race_trainers.`レースキー` = competitors.`レースキー`
-    and race_trainers.`馬番` = competitors.`馬番`
+    and race_trainers.`調教師コード` = competitors.`調教師コード`
   )
-
-select
-  *
-from
-  final
+select * from final
